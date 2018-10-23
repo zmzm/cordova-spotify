199eea1f535e333d82d00061d46618c223383666
Uladzislau Kavalski <uladzislau.kavalski@instinctools.ru>
2018-10-23 18:56:49 +0300

    asdasdasdasd

--- www/build/lib/exec-promise.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Cordova's exec with Promise and error handling support.
- *
- * @param methodName the native method to execute
- * @param args method arguments
- * @hidden
- */
-export default function <T>(methodName: string, args?: any[]): Promise<T>;
--- www/webpack.config.js
+++ www/webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
     ],
     output: {
         path: path.resolve(__dirname, 'build'),
-        filename: 'SpotifyPlugin.js',
+        filename: 'cordova-spotify.min.js',
         library: 'spotify',
         libraryTarget: 'commonjs'
     },
