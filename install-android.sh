983a5fe3cc96d12657e6d865181dff24f6b75d16
Vladislav Kovalskij <vladislav.kovalskij@gmail.com>
2018-10-23 09:17:22 +0300

    Update install-android.sh

--- install-android.sh
+++ install-android.sh
@@ -3,10 +3,6 @@
 SDK_INSTALL_PATH="plugins/cordova-spotify/src/android/spotify-sdk"
 SDK_DOWNLOAD_PATH="https://github.com/spotify/android-streaming-sdk/archive/24-noconnect-2.20b.tar.gz"
 
-cd www
-echo $pwd
-npm i
-
 if [ ! -d "$SDK_INSTALL_PATH" ]; then
     mkdir -p "$SDK_INSTALL_PATH"
     curl -LsS $SDK_DOWNLOAD_PATH | tar -xz -C "$SDK_INSTALL_PATH" --strip 1
